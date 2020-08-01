c574b0ed71bd64573a6b2b9976d18cab5e5315f3
Dino <dino.sources.rex@gmail.com>
2020-08-02 01:59:05 +0200

    Adding content

--- src/app/pages/main/home/child/services/services.component.ts
+++ src/app/pages/main/home/child/services/services.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'qpl-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
 })
 export class ServicesComponent implements OnInit {
+  @Input()
+  service: string;
 
-  constructor() { }
+  altText: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  get imageUrl(): string {
+    switch (this.service) {
+      case 'Plastic Fabrication':
+        return 'assets/services/plastic_fabrication.webp';
+      case 'Bespoke Duct Work':
+        return 'assets/services/bespoke_duct_work.webp';
+      case 'Laboratory Installations':
+        return 'assets/services/laboratory_installations.webp';
+      default:
+        return '';
+    }
   }
 
+  get imageAltText(): string {
+    switch (this.service) {
+      case 'Plastic Fabrication':
+        return 'Photo of some plastic pipes.';
+      case 'Bespoke Duct Work':
+        return 'Photo of some duct work.';
+      case 'Laboratory Installations':
+        return 'Photo of some laboratory chimneys.';
+      default:
+        return 'No image.';
+    }
+  }
 }
--- src/app/pages/main/home/child/testimonial/testimonial.component.ts
+++ src/app/pages/main/home/child/testimonial/testimonial.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'qpl-testimonial',
   templateUrl: './testimonial.component.html',
-  styleUrls: ['./testimonial.component.scss']
+  styleUrls: ['./testimonial.component.scss'],
 })
 export class TestimonialComponent implements OnInit {
+  @Input()
+  company: string;
 
-  constructor() { }
+  @Input()
+  quote: string;
 
-  ngOnInit(): void {
-  }
+  @Input()
+  quotee: string;
 
+  @Input()
+  quoteeTitle: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
